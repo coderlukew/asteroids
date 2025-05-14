916ae4b7266bd7e59b76d337218d7554378aab27
coderlukew <coderluke@yahoo.com>
2025-05-14 14:28:11 -0400

    Setup intial constants and game window

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        pygame.display.flip()  # Update the display
+        clock.tick(60)  # Limit to 60 FPS
+
+    pygame.quit()
+    
+
+
+if __name__ == "__main__":
+    main()
