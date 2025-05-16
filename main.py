a8515c257895bace0396b2c6aacaa0a75cea2513
coderlukew <coderluke@yahoo.com>
2025-05-16 19:22:33 -0400

    Add collison detection

--- main.py
+++ main.py
@@ -33,6 +33,12 @@ def main():
 
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                running = False
+
         screen.fill((0, 0, 0))  # Fill the screen with black
         
         for obj in drawable:
